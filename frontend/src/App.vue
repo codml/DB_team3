676f38406b96de8beb8e587dee3fc39e6b67c732
DwKwCs <kwcs_go670@naver.com>
2024-11-09 15:28:40 +0900

    11.09 - 초기 페이지 디자인. 미완성.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <template>
+  <div class = "main">
+    <router-link to="/main">광운 마켓</router-link>
+  </div>
+  <div class="login">
+    <router-link to="/login">로그인 | 회원가입</router-link>
+  </div>
   <nav>
-    <router-link to="/main">메인페이지</router-link>
-    <router-link to="/login">로그인 페이지</router-link>
-    <router-link to="/search">상품 검색 페이지</router-link>
-    <router-link to="/regist">상품 등록 페이지</router-link>
-    <router-link to="/price">시세 조회 페이지</router-link>
-    <router-link to="/board">자유게시판 페이지</router-link>
+    <router-link to="/search">상품 검색</router-link>
+    <router-link to="/regist">상품 등록</router-link>
+    <router-link to="/price">시세 조회</router-link>
+    <router-link to="/board">자유게시판</router-link>
   </nav>
   <router-view/>
 </template>
@@ -19,14 +23,45 @@
   color: #2c3e50;
 }
 
+div.main a {
+  font-size: 50px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #2c3e50;
+  padding-top: 50px;
+}
+
+div.login a {
+  font-size: 12px;
+  font-weight: bold;
+  text-decoration: none;
+  float: right;
+  margin-right: 300px;
+  color: #2c3e50;
+}
+
+div a.router-link-exact-active {
+  color: #42b983;
+}
+
 nav {
-  padding: 30px;
+  margin: 30px;
+  border-style: solid;
+  border-color: black;
+  border-radius: 10px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 nav a {
+  font-size: 30px;
   font-weight: bold;
+  text-decoration: none;
+  padding: 45px;
   color: #2c3e50;
-  margin-right: 10px; /* 링크 간 간격 설정 */
 }
 
 nav a.router-link-exact-active {
--- frontend/src/pages/main/MainPage.vue
+++ frontend/src/pages/main/MainPage.vue
@@ -3,3 +3,11 @@
     <h1>메인페이지입니다.</h1>
   </div>
 </template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
