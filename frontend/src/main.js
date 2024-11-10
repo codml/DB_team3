12eb8d1b6566d6af69ae73a2100be9b73b08ad14
DwKwCs <kwcs_go670@naver.com>
2024-11-10 21:43:59 +0900

    11.10 - update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Login from './pages/login/LoginPage.vue'
-import SignUP from './pages/signup/SignUPPage.vue'
 import Index from './router/index.js'
 
 createApp(App).use(Index).mount('#app')
-createApp(Login)
-createApp(SignUP)
