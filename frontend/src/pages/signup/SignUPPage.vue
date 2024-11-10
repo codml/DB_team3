60175d6229d4616cd7fdbeeba763f0e3fa6bc57a
DwKwCs <kwcs_go670@naver.com>
2024-11-10 19:25:15 +0900

    11.10 - 로그인, 회원가입 페이지 1

--- frontend/src/pages/signup/SignUPPage.vue
+++ frontend/src/pages/signup/SignUPPage.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="signup">
     <h1>회원가입</h1>
-    <div>
-
-    </div>    
+    <form action="">
+      <div class="input1">
+        <label for="input1">항목1</label><br>
+        <input id="input1" name="input1" type="text" />
+      </div>
+      <div class="input2">
+        <label for="input2">항목2</label><br>
+        <input id="input2" name="input2" type="text" />
+      </div>
+      <div class="input3">
+        <label for="input3">항목3</label><br>
+        <input id="input3" name="input3" type="text" />
+      </div>
+      <div class="input4">
+        <label for="input4">항목4</label><br>
+        <input id="input4" name="input4" type="text" />
+      </div>
+      <div class="input5">
+        <label for="input5">항목5</label><br>
+        <input id="input5" name="input5" type="text" />
+      </div>
+      <button>회원가입</button>
+    </form>    
   </div>
 </template>
 
@@ -15,10 +35,49 @@
 
 .signup {
   margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: solid;
+  border-radius: 10px;
+  width: 500px;
+  height: 580px;
 }
 
 h1 {
   margin: 20px;
+}
+
+div.input1 {
+  margin: 20px;
+}
+
+div.input2 {
+  margin: 20px;
+}
+
+div.input3 {
+  margin: 20px;
+}
+
+div.input4 {
+  margin: 20px;
+}
+
+div.input5 {
+  margin: 20px;
+}
+
+label {
+  font-size: 20px;
+}
+
+input {
+  width: 400px;
+  height: 40px;
+  border-style: solid;
+  border-radius: 5px;
+  border-color: black;
+  font-size: 18px;
 }
 
 </style>
