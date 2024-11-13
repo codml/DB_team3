14a91490c5de02b42babd64feea10b948131029e
JaehyunK <kk2007pp@gmail.com>
2024-11-13 23:47:15 +0900

    designing

--- frontend/src/pages/signup/SignUPPage.vue
+++ frontend/src/pages/signup/SignUPPage.vue
@@ -2,38 +2,38 @@
   <div class="signup">
     <h1>회원가입</h1>
     <form @submit.prevent="handleSubmit">
-      <div class="input1">
+      <div class="information">
          <label for="input1">이름</label><br>
          <input v-model="form.name" id="input1" name="name" type="text" required/>
       </div>
-      <div class="input2">
+      <div class="information">
          <label for="input2">나이</label><br>
          <input v-model="form.age" id="input2" name="age" type="number" min="0" max="200" required/>
       </div>
-      <div class="input3">
+      <div class="information">
          <label>성별</label><br>
          <input v-model="form.gender" id="male" name="gender" type="radio" value="남" checked/>
-         <label for="male">남</label>
+         <label for="male">남성</label>
          <input v-model="form.gender" id="female" name="gender" type="radio" value="여"/>
-         <label for="female">여</label>
+         <label for="female">여성</label>
       </div>
-      <div class="input4">
+      <div class="information">
          <label for="input4">전화번호</label><br>
          <input v-model="form.tel" id="input4" name="tel" type="text" required/>
       </div>
-      <div class="input5">
+      <div class="information">
          <label for="input5">이메일</label><br>
          <input v-model="form.email" id="input5" name="email" type="text" required/>
       </div>
-      <div class="input6">
+      <div class="information">
          <label for="input6">주소</label><br>
          <input v-model="form.address" id="input6" name="address" type="text" required/>
       </div>
-      <div class="input7">
+      <div class="information">
          <label for="input7">아이디</label><br>
          <input v-model="form.id" id="input7" name="id" type="text" required/>
       </div>
-      <div class="input8">
+      <div class="information">
          <label for="input8">비밀번호</label><br>
          <input v-model="form.passwd" id="input8" name="passwd" type="text" required/>
       </div><br>
@@ -99,32 +99,20 @@ h1 {
   margin: 20px;
 }
 
-div.input1 {
-  margin: 20px;
-}
-
-div.input2 {
-  margin: 20px;
-}
-
-div.input3 {
-  margin: 20px;
-}
-
-div.input4 {
-  margin: 20px;
-}
-
-div.input5 {
-  margin: 20px;
+.information {
+  margin-top: 30px;
+  margin-left: 100px;
+  margin-right: 100px;
 }
 
 label {
-  font-size: 20px;
-}
+	font-size: 20px;
+    text-align: left; /* 텍스트 왼쪽 정렬 */
+    display: block;   /* label을 블록 요소로 설정하여 전체 너비를 차지하게 함 */
+  }
 
 input {
-  width: 400px;
+  width: 300px;
   height: 40px;
   border-style: solid;
   border-radius: 5px;
