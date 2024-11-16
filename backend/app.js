5c1e1499c7b79c047f0ddb10267cf06c63b19da9
DwKwCs <kwcs_go670@naver.com>
2024-11-16 15:43:40 +0900

    11.16 - 백엔드 market 라우터 생성

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var marketRouters = require('./routes/market');
 
 var app = express();
 
@@ -19,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', marketRouters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
