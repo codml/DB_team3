367e4cbffbecd1c5579f427b485dff63cea4cdfa
Dongwoo Go <90742967+DwKwCs@users.noreply.github.com>
2024-11-18 18:24:32 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser');
 
 var marketRouters = require('./routes/market');
 
+var signupRouters = require('./routes/signup');
+var writepostRouters = require('./routes/writepost'); var boardpageRouters = require('./routes/boardpage'); // boardpage 라우트 추가
+
 var app = express();
 
 // view engine setup
