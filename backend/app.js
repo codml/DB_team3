27af96af7d0d2e84cce65e84a5d2610c73ba5ef9
Dongwoo Go <90742967+DwKwCs@users.noreply.github.com>
2024-11-18 18:35:36 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', marketRouters);
+app.use('/signup', signupRouters);
+app.use('/search', searchRouters);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
