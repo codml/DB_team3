a753cf2c00a103e86dbf103c547bcb9e4023409d
JaehyunK <kk2007pp@gmail.com>
2024-11-18 23:29:26 +0900

    11-18 add signup error message

--- backend/controllers/signupController.js
+++ backend/controllers/signupController.js
@@ -2,7 +2,14 @@ var userModel = require('../models/userModel');
 
 exports.insertUser = (req, res, next) => {
 	console.log("req: " + JSON.stringify(req.body));
-	userModel.insertUser(req.body, (rows) => {
-		res.json(rows);
-	})
+    userModel.insertUser(req.body, (status) => {
+        if (status === 'DB_DUPLICATE_ERROR') {
+            res.status(409).json(status);
+        } else if(status === 'DB_ERROR'){
+			res.status(500).json(status);
+        }
+		else{
+            res.status(200).json(status); // 실패 메시지 반환
+		}
+    });
 }
